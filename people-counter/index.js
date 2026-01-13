c8ed8bc00c0dfd6f1cdc54ae6cec6fe37c9882bf
JMTC-dev <jackmatthewcasey@gmail.com>
2026-01-13 21:46:31 +1000

    goblins

--- people-counter/index.js
+++ people-counter/index.js
@@ -1,10 +1,16 @@
 // document.getElementById("count").innerText = 5;
 
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 function increment() {
-  count = count + 1;
-  countEl.innerText = count;
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let saveValue = count + " - ";
+  saveEl.textContent += " " + saveValue;
 }
