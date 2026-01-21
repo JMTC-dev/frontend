c8a213650b301ae9fb090906fbe14beb1d784fa3
JMTC-dev <jackmatthewcasey@gmail.com>
2026-01-21 15:12:19 +1000

    styling

--- blackjack/index.js
+++ blackjack/index.js
@@ -3,10 +3,15 @@ let secondCard = 10;
 
 let sum = firstCard + secondCard;
 
+let hasBlackJack = false;
+let isAlive = true;
+
 if (sum <= 20) {
   console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
   console.log("You've won!");
+  hasBlackJack = true;
 } else {
   console.log("You've lost!");
+  isAlive = false;
 }
